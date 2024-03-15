6ee77094f95f160cba588af42dafae96e58d0111
Shoubhit Dash <shoubhit2005@gmail.com>
2024-03-15 18:32:32 +0530

    redesign db schema

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -5,14 +5,25 @@ import {
   timestamp,
   text,
   pgEnum,
+  integer,
 } from "drizzle-orm/pg-core";
 
 export const roleEnum = pgEnum("role", ["system", "user"]);
 
-export const messages = pgTable("messages", {
+export const entries = pgTable("messages", {
   id: serial("id").primaryKey(),
   createdAt: timestamp("created_at").notNull().defaultNow(),
   userId: varchar("user_id", { length: 256 }).notNull(),
   content: text("content").notNull(),
   role: roleEnum("role").notNull(),
+  tagId: integer("tag_id")
+    .references(() => tags.id)
+    .notNull(),
+});
+
+export const tags = pgTable("tags", {
+  id: serial("id").primaryKey(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  userId: varchar("user_id", { length: 256 }).notNull(),
+  name: varchar("name", { length: 256 }).notNull(),
 });
