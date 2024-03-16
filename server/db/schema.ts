73483902772043fcd1cd12409f4b10093e1bc63b
Shoubhit Dash <shoubhit2005@gmail.com>
2024-03-16 15:47:04 +0530

    update schema

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -9,21 +9,12 @@ import {
 } from "drizzle-orm/pg-core";
 
 export const roleEnum = pgEnum("role", ["system", "user"]);
+export const typeEnum = pgEnum("type", ["todo", "note"]);
 
-export const entries = pgTable("entries", {
+export const notes = pgTable("notes", {
   id: serial("id").primaryKey(),
   createdAt: timestamp("created_at").notNull().defaultNow(),
   userId: varchar("user_id", { length: 256 }).notNull(),
   content: text("content").notNull(),
-  role: roleEnum("role").notNull(),
-  tagId: integer("tag_id")
-    .references(() => tags.id)
-    .notNull(),
-});
-
-export const tags = pgTable("tags", {
-  id: serial("id").primaryKey(),
-  createdAt: timestamp("created_at").notNull().defaultNow(),
-  userId: varchar("user_id", { length: 256 }).notNull(),
-  name: varchar("name", { length: 256 }).notNull(),
+  type: typeEnum("type"),
 });
