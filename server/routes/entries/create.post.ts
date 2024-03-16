7237cd949f1d3baad0ae145f7a97f4f58091ce58
Shoubhit Dash <shoubhit2005@gmail.com>
2024-03-16 18:42:18 +0530

    refactor creation

--- server/routes/entries/create.post.ts
+++ server/routes/entries/create.post.ts
@@ -1,12 +1,13 @@
 import { ChatOpenAI, OpenAIEmbeddings } from "@langchain/openai";
 import { PineconeStore } from "@langchain/pinecone";
 import { Pinecone } from "@pinecone-database/pinecone";
-import { ChatPromptTemplate } from "langchain/prompts";
-import { StringOutputParser } from "langchain/schema/output_parser";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { StringOutputParser } from "@langchain/core/output_parsers";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { z } from "zod";
 import { db } from "../../db";
 import { notes } from "../../db/schema";
+import { InferSelectModel } from "drizzle-orm";
 
 export default eventHandler(async (event) => {
   const bodySchema = z.object({
@@ -28,25 +29,6 @@ export default eventHandler(async (event) => {
 
   const { content, userId } = parsedBodyResult.data;
 
-  const splitter = RecursiveCharacterTextSplitter.fromLanguage("markdown", {
-    chunkSize: 500,
-    chunkOverlap: 0,
-  });
-
-  const docs = await splitter.createDocuments([content]);
-  const embeddings = new OpenAIEmbeddings({
-    modelName: "text-embedding-3-large",
-  });
-
-  const pinecone = new Pinecone();
-  const pineconeIndex = pinecone.index("puno");
-
-  await PineconeStore.fromDocuments(docs, embeddings, {
-    pineconeIndex,
-    namespace: userId,
-    maxConcurrency: 5,
-  });
-
   const model = new ChatOpenAI({ modelName: "gpt-4-turbo-preview" });
 
   const prompt = ChatPromptTemplate.fromMessages([
@@ -54,7 +36,7 @@ export default eventHandler(async (event) => {
       "system",
       `You are an excellent distinguisher between two categories of notes: quick notes and todos.
   Todos have a direct tone and imply that a task has to be done. For example: "Finish english assignment".
-  Quick notes could be anything from anything from fun facts to events happening around the user. For example: "I learnt that Kung Pao Chicken has peanuts in it" or "I watched Interstellar with Karan today".
+  Quick notes could be anything from fun facts to events happening around the user. For example: "I learnt that Kung Pao Chicken has peanuts in it" or "I watched Interstellar with Karan today".
   Reply with ONLY "yes" or "no". Reply with "yes" if it's a todo, else reply with "no" if it's a quick note. DO NOT REPLY WITH ANYTHING OTHER THAN "yes" or "no".
   `,
     ],
@@ -70,9 +52,37 @@ export default eventHandler(async (event) => {
   });
 
   if (todo === "yes") {
-    await db.insert(notes).values({ userId, content, type: "todo" });
+    await db
+      .insert(notes)
+      .values({ userId, content, type: "todo" })
+      .returning({ id: notes.id });
   } else if (todo === "no") {
-    await db.insert(notes).values({ userId, content, type: "note" });
+    const noteId = await db
+      .insert(notes)
+      .values({ userId, content, type: "note" })
+      .returning({ id: notes.id });
+
+    const splitter = RecursiveCharacterTextSplitter.fromLanguage("markdown", {
+      chunkSize: 500,
+      chunkOverlap: 0,
+    });
+
+    const docs = await splitter.createDocuments(
+      [content],
+      [{ noteId: noteId[0].id }]
+    );
+    const embeddings = new OpenAIEmbeddings({
+      modelName: "text-embedding-3-large",
+    });
+
+    const pinecone = new Pinecone();
+    const pineconeIndex = pinecone.index("puno");
+
+    await PineconeStore.fromDocuments(docs, embeddings, {
+      pineconeIndex,
+      namespace: userId,
+      maxConcurrency: 5,
+    });
   } else {
     throw new Error("Invalid response for todo classification");
   }
