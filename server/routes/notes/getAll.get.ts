f1eec6bcd10c26f9c02e095869a14c404454e505
Shoubhit Dash <shoubhit2005@gmail.com>
2024-03-17 03:01:12 +0530

    fuck it push to prod pt.2

---
+++ server/routes/notes/getAll.get.ts
@@ -0,0 +1,28 @@
+import { z } from "zod";
+import { db } from "../../db";
+import { notes } from "../../db/schema";
+import { eq } from "drizzle-orm";
+
+export default defineEventHandler(async (event) => {
+  const bodySchema = z.object({
+    userId: z.string(),
+  });
+
+  const result = getQuery(event);
+
+  if (!result) {
+    throw new Error("fail lol");
+  }
+
+  console.log(result.userId);
+
+  const parsedBodyResult = bodySchema.safeParse(result);
+
+  if (!parsedBodyResult.success) {
+    throw new Error("Invalid body format");
+  }
+
+  const { userId } = parsedBodyResult.data;
+
+  return await db.select().from(notes).where(eq(notes.userId, userId));
+});
