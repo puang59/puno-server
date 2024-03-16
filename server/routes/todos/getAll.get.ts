7e703583d6f3065526f1da0bd80ccb055591977b
Shoubhit Dash <shoubhit2005@gmail.com>
2024-03-17 02:42:30 +0530

    refactor

--- server/routes/todos/getAll.get.ts
+++ server/routes/todos/getAll.get.ts
@@ -1,6 +1,26 @@
+import { z } from "zod";
 import { db } from "../../db";
 import { todos } from "../../db/schema";
+import { eq } from "drizzle-orm";
 
 export default defineEventHandler(async (event) => {
-  return await db.select().from(todos);
+  const bodySchema = z.object({
+    id: z.number(),
+  });
+
+  const result = getQuery(event);
+
+  if (!result) {
+    throw new Error("fail lol");
+  }
+
+  const parsedBodyResult = bodySchema.safeParse(result);
+
+  if (!parsedBodyResult.success) {
+    throw new Error("Invalid body format");
+  }
+
+  const { id } = parsedBodyResult.data;
+
+  return await db.select().from(todos).where(eq(todos.id, id));
 });
